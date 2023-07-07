811543eaa5965afc50f6c576023a4109263bdcb3
miguelhhuerto <miguelhuerto@gmail.com>
2023-07-07 15:05:17 +0800

    Created New Post Validation and Error Messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
-    post = Post.new(params[:post].permit(:title, :content, :address))
-    if post.save
+    @post = Post.new(params[:post].permit(:title, :content, :address))
+    if @post.save
+      flash[:notice] = 'Post created successfully'
       redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post create failed'
+      render :new, status: :unprocessable_entity
     end
   end
 end
