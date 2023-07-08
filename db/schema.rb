af9a6d7400ac01f42a84cfe9e9dd16ce12ee52da
miguelhhuerto <miguelhhuerto@gmail.com>
2023-07-08 22:01:48 +0800

    Generated Category and PostCategoryShips Models and Migration Files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_081803) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_134633) do
+  create_table "categories", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_category_ships", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_post_category_ships_on_category_id"
+    t.index ["post_id"], name: "index_post_category_ships_on_post_id"
+  end
+
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "content"
