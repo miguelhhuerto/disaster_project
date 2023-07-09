5dffc6cc7c3f9f3886c90b65202f526400e1587e
miguelhhuerto <miguelhhuerto@gmail.com>
2023-07-10 00:04:56 +0800

    Fixed Seeds Typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 
-10.times do
-    user = User.create!(email: Faker::Internet.email, password: 'qwer4321', password_confirmation: "qwer4321")
-    puts "create user id: #{user.id}, email: #{user.email}"
-end
+# 10.times do
+#     user = User.create!(email: Faker::Internet.email, password: 'qwer4321', password_confirmation: "qwer4321")
+#     puts "create user id: #{user.id}, email: #{user.email}"
+# end
 
 30.times do |i|
     puts "start create #{i} post"
-    post = Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user: User.all.sample)
+    post = Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, address: Faker::Lorem.sentence,user: User.all.sample)
     (1..20).to_a.sample.times do
         Comment.create(content: Faker::Lorem.sentence, user: User.all.sample, post: post)
     end
