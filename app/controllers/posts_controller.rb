5578ac7e6cd7978dd3d4e3c744da66a980a34237
miguelhhuerto <miguelhhuerto@gmail.com>
2023-07-09 22:09:23 +0800

    Remove Order Posts in Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @posts = Post.includes(:categories, :user).page(params[:page]).per(5).order(comments_count: :desc)
+    @posts = Post.includes(:categories, :user).page(params[:page]).per(5)
   end
 
   def new
