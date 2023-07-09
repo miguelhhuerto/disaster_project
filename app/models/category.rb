f4ec58dee83f8b701bb9ebf95509899e0999bef8
miguelhhuerto <miguelhhuerto@gmail.com>
2023-07-09 21:47:50 +0800

    Added limitation to Categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
     validates :name, presence: true
+    validates_associated :category
+
     has_many :post_category_ships
     has_many :posts, through: :post_category_ships
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,15 +4,16 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :address, presence: true
+  validates_length_of :categories, maximum: 3
 
   has_many :comments
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
+  has_one :photo
   belongs_to :user
+  
   mount_uploader :image, ImageUploader
-
   def destroy
     update(deleted_at: Time.now)
   end
-
 end
